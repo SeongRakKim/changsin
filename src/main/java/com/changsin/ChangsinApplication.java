b9ca3cd73c2144bdcd86cb5b40377a58d0734853
SeongNakKim <dlsnzkdl@gmail.com>
2022-02-09 23:54:46 +0900

    Jsp 이동...

--- src/main/java/com/changsin/ChangsinApplication.java
+++ src/main/java/com/changsin/ChangsinApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = "com.changsin.webapp.mes")
 public class ChangsinApplication {
 
 	public static void main(String[] args) {
