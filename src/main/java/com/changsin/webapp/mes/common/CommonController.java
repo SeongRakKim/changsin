ffda08afb2754cb33b7a7492a9c8b11cb3a7fbbc
SeongRakKim <dlsnzkdl2@gmail.com>
2022-02-10 18:19:47 +0900

    templete

---
+++ src/main/java/com/changsin/webapp/mes/common/CommonController.java
@@ -0,0 +1,21 @@
+package com.changsin.webapp.mes.common;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+public class CommonController {
+
+    @GetMapping("/")
+    public String index() throws Exception {
+        return "index";
+    }
+
+
+}
