cff6921415eb69af1cd8585d4ddcb551f963e36c
SeongRakKim <dlsnzkdl2@gmail.com>
2022-02-28 19:39:50 +0900

    templete

--- src/main/resources/static/common/common.js
+++ src/main/resources/static/common/common.js
@@ -108,6 +108,18 @@ function setDataOne(prefix, data) {
 	});
 }
 
+function getDatesStartToLast(startDate, lastDate) {
+	var regex = RegExp(/^\d{4}-(0[1-9]|1[012])-(0[1-9]|[12][0-9]|3[01])$/);
+	if(!(regex.test(startDate) && regex.test(lastDate))) return "Not Date Format";
+	var result = [];
+	var curDate = new Date(startDate);
+	while(curDate <= new Date(lastDate)) {
+		result.push(curDate.toISOString().split("T")[0]);
+		curDate.setDate(curDate.getDate() + 1);
+	}
+	return result;
+}
+
 function IsNotNull(el) {
 	if (el == null) return false;
 	if (el == "NaN") return false;
